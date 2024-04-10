3bb4c51eeaa2ffb3dcaf770d1afcc479c54a8fbc
Sai Vamshi <90269703+VamshiVar@users.noreply.github.com>
2024-04-10 17:19:55 +0530

    Update delay.js

--- delay.js
+++ delay.js
@@ -24,46 +24,28 @@ async function delayTest() {
     const config = JSON.parse(data);
     console.log(`TestName is: ${config.testName}`);
 
-    const response = await fetch('https://1b0y3delb4.execute-api.us-west-2.amazonaws.com/prod/scenarios/');
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data`);
-    }
-    const testData = await response.json();
-    const test = testData.Items.find(item => item.testName === config.testName);
-    if (!test) {
-      throw new Error(`Test with name "${config.testName}" not found`);
-    }
-    console.log(`Test ID: ${test.testId}`);
-
     const apiUrl = 'https://1b0y3delb4.execute-api.us-west-2.amazonaws.com/prod/scenarios';
     let status = 'RUNNING';
 
-    while (status === 'RUNNING' || status === 'Queued') {
-      status = await checkTestStatus(apiUrl, test.testId);
+    while (true) {
+      status = await checkTestStatus(apiUrl, config.testId);
       console.log(`Test status: ${status}`);
       
       if (status === 'RUNNING' || status === 'Queued') {
         console.log('The test is RUNNING');
-        await delay(2 * 60 * 1000); // Wait for 2 minutes before checking again
-      } else if (status === 'Failed') {
-        console.log('The test has failed');
+      } else if (status === 'Failed' || status === 'COMPLETE') {
+        console.log(`The test has ${status}`);
         console.log('Fetching results...');
-        const resultResponse = await fetch(`${apiUrl}/${test.testId}`);
+        const resultResponse = await fetch(`${apiUrl}/${config.testId}`);
         if (!resultResponse.ok) {
-          throw new Error(`Failed to fetch test result for test ID ${test.testId}`);
+          throw new Error(`Failed to fetch test result for test ID ${config.testId}`);
         }
         const resultData = await resultResponse.json();
         console.log('Test results:', resultData);
-      } else if (status === 'COMPLETE') {
-        console.log('The test has COMPLETE');
-        console.log('Fetching results...');
-        const resultResponse = await fetch(`${apiUrl}/${test.testId}`);
-        if (!resultResponse.ok) {
-          throw new Error(`Failed to fetch test result for test ID ${test.testId}`);
-        }
-        const resultData = await resultResponse.json();
-        console.log('Test results:', resultData);
+        break; // Exit the loop when the test status is not RUNNING or Queued
       }
+      
+      await delay(2 * 60 * 1000); // Wait for 2 minutes before checking again
     }
   } catch (err) {
     console.error("Error:", err);
