4a6628e2cd4e563e264517e6e7255af11d34c03e
Sai Vamshi <90269703+VamshiVar@users.noreply.github.com>
2024-04-10 15:31:05 +0530

    Update delay.js

--- delay.js
+++ delay.js
@@ -5,13 +5,13 @@ async function delayTest() {
   try {
     const data = await fs.readFile('test-config.json', 'utf8');
     const config = JSON.parse(data);
-    console.log(`waiting for ${config.hold_for} min(s)`); // Use backticks for template literals
-    setTimeout(async () => {
-      const url = `${config.api_url}${config.test_id}`; // Use backticks for template literals
-      const response = await fetch(url);
-      const result = await response.json();
-      console.log('Data results from API:', result);
-    }, config.hold_for * 60 * 1000);
+    console.log(`TestName is: ${config.testName}`); // Use backticks for template literals
+    // setTimeout(async () => {
+    //   const url = `${config.api_url}${config.test_id}`; // Use backticks for template literals
+    //   const response = await fetch(url);
+    //   const result = await response.json();
+    //   console.log('Data results from API:', result);
+    // }, config.hold_for * 60 * 1000);
   } catch (err) {
     console.error("Error:", err); // Fix the syntax error here
   }
