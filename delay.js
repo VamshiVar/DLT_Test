4eb0760e7d41ad06cbe88b4f57690787f0e82046
Sai Vamshi <90269703+VamshiVar@users.noreply.github.com>
2024-04-10 15:46:40 +0530

    Update delay.js

--- delay.js
+++ delay.js
@@ -6,6 +6,19 @@ async function delayTest() {
     const data = await fs.readFile('test-config.json', 'utf8');
     const config = JSON.parse(data);
     console.log(`TestName is: ${config.testName}`); // Use backticks for template literals
+
+    const response = await fetch('https://1b0y3delb4.execute-api.us-west-2.amazonaws.com/prod/scenarios/');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data`);
+    }
+    const testData = await response.json();
+    const test = testData.Items.find(item => item.testName === '${config.testName}');
+    if (test) {
+      return test.testId;
+      console.log(`test id is: "${testId}" `);
+    } else {
+      throw new Error(`Failed to fetch test name "${testname}" `);
+    }
     // setTimeout(async () => {
     //   const url = `${config.api_url}${config.test_id}`; // Use backticks for template literals
     //   const response = await fetch(url);
@@ -14,6 +27,7 @@ async function delayTest() {
     // }, config.hold_for * 60 * 1000);
   } catch (err) {
     console.error("Error:", err); // Fix the syntax error here
+    return null;
   }
 }
 
